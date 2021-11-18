6ef5f0a013d95802c7c5fe03361520f43acf9d54
Catherine Hodgkinson <catherinemhodgkinson@gmail.com>
2021-11-17 23:20:14 -0500

    commit tree 168c491f4d4fd89065bc030b8955b8580ca36128

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,21 @@
-export function manageFriends(state, action){
+export function manageFriends(state={
+    friends: []
+}, action) {
+    switch (action.type) {
+        case "ADD_FRIEND":
+            return { ...state, friends: [ ...state.friends, action.friend ]};
+        case "REMOVE_FRIEND":
+            const remover = state.friends.findIndex(friend => friend.id === action.id)
+            return {  ...state, friends: [...state.friends.slice(0, remover),
+                    ...state.friends.slice(remover + 1)]};
+        default:
+            return state;
+    }
 }
+
+
+
+
+// friends: [
+//     ...state.friends.slice(0, removalIndex), slice off first through one being removed
+//     ...state.friends.slice(removalIndex + 1) slice off last one (one being removed) through the end (aka just that last one)
